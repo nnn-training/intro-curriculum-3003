38913dde82662aa86f0a23d7c778b4f3d9b32f9d
akanekwom172 <akanekwon_dev@icloud.com>
2021-08-31 07:21:55 +0900

    逆順のランキングを実装

--- app.js
+++ app.js
@@ -2,7 +2,7 @@
 const fs = require('fs');
 const readline = require('readline');
 const rs = fs.createReadStream('./popu-pref.csv');
-const rl = readline.createInterface({ 'input': rs, 'output': {} });
+const rl = readline.createInterface({ input: rs, output: {} });
 const prefectureDataMap = new Map(); // key: 都道府県 value: 集計データのオブジェクト
 rl.on('line', (lineString) => {
   const columns = lineString.split(',');
@@ -28,14 +28,17 @@ rl.on('line', (lineString) => {
   }
 });
 rl.on('close', () => {
-  for (let [key, value] of prefectureDataMap) { 
+  for (let [key, value] of prefectureDataMap) {
     value.change = value.popu15 / value.popu10;
   }
   const rankingArray = Array.from(prefectureDataMap).sort((pair1, pair2) => {
-    return pair2[1].change - pair1[1].change;
+    return pair1[1].change - pair2[1].change;
   });
-  const rankingStrings = rankingArray.map(([key, value]) => {
-    return key + ': ' + value.popu10 + '=>' + value.popu15 + ' 変化率:' + value.change;
+  const rankingStrings = rankingArray.map(([key, value], i) => {
+    const [popu10, popu15] = [value.popu10.toString(), value.popu15.toString()];
+    return `${(i + 1).toString().padStart(2, '0')}位` +
+      ` ${key.padStart(4, '\u3000')}:${popu10.padStart(6)}=>${popu15.padStart(6)}` +
+      ` 変化率:${value.change}`;
   });
   console.log(rankingStrings);
 });
