6c6ac4ed82fb64d14975d2db105105c82895c125
ibuki <ibuki-taguchi@cybozu.co.jp>
2021-09-13 13:01:58 +0900

    昇順のランキングと順位の表示を実装

--- app.js
+++ app.js
@@ -1,11 +1,11 @@
-'use strict';
-const fs = require('fs');
-const readline = require('readline');
-const rs = fs.createReadStream('./popu-pref.csv');
-const rl = readline.createInterface({ 'input': rs, 'output': {} });
+"use strict";
+const fs = require("fs");
+const readline = require("readline");
+const rs = fs.createReadStream("./popu-pref.csv");
+const rl = readline.createInterface({ input: rs, output: {} });
 const prefectureDataMap = new Map(); // key: 都道府県 value: 集計データのオブジェクト
-rl.on('line', (lineString) => {
-  const columns = lineString.split(',');
+rl.on("line", (lineString) => {
+  const columns = lineString.split(",");
   const year = parseInt(columns[0]);
   const prefecture = columns[1];
   const popu = parseInt(columns[3]);
@@ -15,7 +15,7 @@ rl.on('line', (lineString) => {
       value = {
         popu10: 0,
         popu15: 0,
-        change: null
+        change: null,
       };
     }
     if (year === 2010) {
@@ -27,15 +27,17 @@ rl.on('line', (lineString) => {
     prefectureDataMap.set(prefecture, value);
   }
 });
-rl.on('close', () => {
+rl.on("close", () => {
   for (const [key, value] of prefectureDataMap) {
     value.change = value.popu15 / value.popu10;
   }
-  const rankingArray = Array.from(prefectureDataMap).sort((pair1, pair2) => {
-    return pair2[1].change - pair1[1].change;
+  // ソート(昇順)
+  const rankingArray2 = Array.from(prefectureDataMap).sort((pair1, pair2) => {
+    return (pair2[1].change - pair1[1].change) * -1;
   });
-  const rankingStrings = rankingArray.map(([key, value]) => {
-    return key + ': ' + value.popu10 + '=>' + value.popu15 + ' 変化率:' + value.change;
+  const rankingStrings2 = rankingArray2.map(([k, v], i) => {
+    // 出力(昇順))
+    return `${i + 1}位:${k}: ${v.popu10} => ${v.popu15} 変化率:${v.change}`;
   });
-  console.log(rankingStrings);
+  console.log(rankingStrings2);
 });
