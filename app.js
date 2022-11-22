91e6b3471b298f18380414687685893226563974
Jun <jun@example.com>
2022-11-22 13:34:46 +0900

    Change the ranking order to descending

--- app.js
+++ app.js
@@ -33,9 +33,9 @@ rl.on('close', () => {
   }
   const rankingArray = Array.from(prefectureDataMap).sort((pair1, pair2) => {
     return pair2[1].change - pair1[1].change;
-  });
-  const rankingStrings = rankingArray.map(([key, value]) => {
-    return `${key}: ${value.popu10}=>${value.popu15} 変化率: ${value.change}`;
+  }).reverse();
+  const rankingStrings = rankingArray.map(([key, value], index) => {
+    return `${index + 1}位 ${key}: ${value.popu10}=>${value.popu15} 変化率: ${value.change}`;
   });
   console.log(rankingStrings);
 });
