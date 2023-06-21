3a6a6da9e660eb1d57fb828a66f3a9b4f3a3d39d
Mio Harada <mio.rabbit971.snow@gmail.com>
2023-06-21 15:00:42 +0900

    変更

--- app.js
+++ app.js
@@ -1,11 +1,11 @@
 'use strict';
 const fs = require('node:fs');
-const readline = require('readline');
+const readline = require('node:readline');
 const rs = fs.createReadStream('./popu-pref.csv');
 const rl = readline.createInterface({ input: rs });
 const prefectureDataMap = new Map(); // key: 都道府県 value: 集計データのオブジェクト
 rl.on('line', lineString => {
-  const columns = lineString.split(',');
+	const columns = lineString.split(',');
   const year = parseInt(columns[0]);
   const prefecture = columns[1];
   const popu = parseInt(columns[3]);
@@ -30,14 +30,14 @@ rl.on('line', lineString => {
   }
 });
 rl.on('close', () => {
-  for (const [key, value] of prefectureDataMap) {
+	for (const [key, value] of prefectureDataMap) {
     value.change = value.after / value.before;
   }
-  const rankingArray = Array.from(prefectureDataMap).sort((pair1, pair2) => {
-    return pair2[1].change - pair1[1].change;
+	const rankingArray = Array.from(prefectureDataMap).sort((pair1, pair2) => {
+    return pair1[1].change - pair2[1].change;
   });
-  const rankingStrings = rankingArray.map(([key, value]) => {
-    return `${key}: ${value.before}=>${value.after} 変化率: ${value.change}`;
+	const rankingStrings = rankingArray.map(([key, value], i) => {
+    return `${i + 1}位 ${key}: ${value.before}=>${value.after} 変化率: ${value.change}`;
   });
   console.log(rankingStrings);
 });
