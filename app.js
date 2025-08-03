635ee486939441fe0b1e5f53bb397d8645e784db
Kyoko Kanazawa <k.kyoko.c@gmail.com>
2025-08-03 21:22:30 +0900

    人が減った割合のランキング

--- app.js
+++ app.js
@@ -33,11 +33,13 @@ rl.on('close', () => {
   for (const [key, value] of prefectureDataMap) {
     value.change = value.after / value.before;
   }
-  const rankingArray = Array.from(prefectureDataMap).sort((pair1, pair2) => {
+  //正負を入れ替えて逆順にソート
+  const rankingArray = Array.from(prefectureDataMap).sort((pair2, pair1) => {
     return pair2[1].change - pair1[1].change;
   });
-  const rankingStrings = rankingArray.map(([key, value]) => {
-    return `${key}: ${value.before}=>${value.after} 変化率: ${value.change}`;
+  //ランキングの順位
+  const rankingStrings = rankingArray.map(([key, value], i ) => {
+    return ` ${i + 1}位: ${key}: ${value.before}=>${value.after} 変化率: ${value.change}`;
   });
   console.log(rankingStrings);
 });
